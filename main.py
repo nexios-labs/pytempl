3c5b3c32ed476aadb3e34ef5cdda9a4d3f9cbdf1
Mohammed Al-Ameen <ameenmohammed2311@gmail.com>
2025-09-19 19:40:40 +0100

    Docs/examples (#9)
    * fix: adds text content to for loop target
    
    * docs: adds demo to examples

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from templ.engine import Engine
 
 def main():
     engine = Engine()
-    engine.render("./sample.pytempl")
+    engine.render("./examples/demo.pytempl", format=True)
+    engine.render("./examples/sample.pytempl", format=True)
 
 
 if __name__ == "__main__":
