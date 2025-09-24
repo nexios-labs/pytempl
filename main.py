75d24b635e4868c132cef295a73ff6ec96856f7b
Mohammed Al-Ameen <ameenmohammed2311@gmail.com>
2025-09-24 17:30:01 +0100

    docs: adds ssg sample

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ def main():
     engine = Engine()
     engine.render("./examples/demo.pytempl", format=True)
     engine.render("./examples/sample.pytempl", format=True)
+    engine.ssg("./examples/blog")
 
 
 if __name__ == "__main__":
