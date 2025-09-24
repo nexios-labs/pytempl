db1a834daa5ff0119be60f6c56e0e550cf453d18
Mohammed Al-Ameen <ameenmohammed2311@gmail.com>
2025-09-24 17:28:58 +0100

    feat: adds ssg method to engine

--- templ/engine.py
+++ templ/engine.py
@@ -1,3 +1,6 @@
+import importlib
+import os
+
 import ruff_api
 from lark import Lark, Token, Tree
 
@@ -37,3 +40,22 @@ class Engine:
     def save(self, file_name: str, content: str):
         with open(file_name, "w") as f:
             f.write(content)
+
+    def _scan_directory(self, template_dir: str):
+        for entry in os.listdir(template_dir):
+            full_path = os.path.join(template_dir, entry)
+            if os.path.isdir(full_path):
+                yield from self._scan_directory(full_path)
+            else:
+                if full_path.endswith(".pytempl"):
+                    yield full_path
+
+    def ssg(self, template_dir: str):
+        templates = list(self._scan_directory(template_dir))
+        for template in templates:
+            self.render(template, True, True)
+            template_module = importlib.import_module(
+                template.replace(".pytempl", "").replace("./", "").replace("/", ".")
+            )
+            if hasattr(template_module, "Page"):
+                self.save(template.replace(".pytempl", ".html"), template_module.Page())
